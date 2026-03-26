32dde7dce24b940f2fd6bd3d0e52cc10387b8c67
kirin <limkirin2004@gmail.com>
2026-03-26 11:01:24 +0800

    Uploaded completed code with updated README

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 ### This project is aimed at helping me apply the skills I have learnt on beginner python libraries like matplotlib and pandas. This is my first personal project and I want to learn how to apply the skills I've learnt in my course in something that is more practical in the work force.
 
-## The project is a sales dashboard that will read sales data from a CSV file, and show the following data:
-- total sales
-- total sales by product
-- sales by month
-- plot charts based on said sales
+# Sales Dashboard
 
+This project analyzes sales data using Python, pandas, and matplotlib.
+
+## Features
+- Calculates total sales and units
+- Compares sales by product, month, and region
+- Visualizes data using charts
+
+## Tools Used
+- Python
+- pandas
+- matplotlib
--- main.py
+++ main.py
@@ -6,3 +6,56 @@ df = pd.read_csv("sales.csv")
 
 print("Sales Data:")
 print(df.head())
+
+#metrics to display
+total_sales = df["Sales"].sum()
+total_units = df["Units"].sum()
+average_sales = df["Sales"].mean()
+
+print("\n---Summary---")
+print(f"Total Sales: {total_sales}")
+print(f"Total Units sold: {total_units}")
+print(f"Average Monthly Sales: {average_sales:.2f}")    #rounded off the the monthly sales
+
+#Sales by product
+sales_by_product = df.groupby("Product")["Sales"].sum()
+print("\nSales by Product:")
+print(sales_by_product)
+
+#Sales by month
+sales_by_month = df.groupby("Month")["Sales"].sum()
+print("\nSales by Month:")
+print(sales_by_month)
+
+#Sales by region
+sales_by_region = df.groupby("Region")["Sales"].sum()
+print("\nSales by Region:")
+print(sales_by_region)
+
+# chart 1: Sales by product 
+plt.figure(figsize = (8, 5))
+plt.bar(sales_by_product.index, sales_by_product.values)
+plt.title("Sales by Product")
+plt.xlabel("Product")
+plt.ylabel("Sales")
+plt.tight_layout()
+plt.show()
+
+#chart 2: Sales by month
+plt.figure(figsize = (8, 5))
+plt.bar(sales_by_month.index, sales_by_month.values)
+plt.title("Sales by Month")
+plt.xlabel("Month")
+plt.ylabel("Sales")
+plt.tight_layout()
+plt.show()
+
+#chart 3: Sales by region
+plt.figure(figsize = (8, 5))
+plt.bar(sales_by_region.index, sales_by_region.values)
+plt.title("Sales by Region")
+plt.xlabel("Region")
+plt.ylabel("Sales")
+plt.tight_layout()
+plt.show()
+
